eea4c3b1a35ceb5250a1b3c138ea1184db1e28a3
vladbaldin666 <vladbaldin666@mail.ru>
2017-02-19 18:09:31 +0300

    test1

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -9,7 +9,14 @@
 
 TBitField::TBitField(int len)
 {
+	if (len < 1) throw BADSIZE;
+	BitLen = len;
+	MemLen = BitLen / sizeof(TELEM) * 8 + 1;
+	pMem = new TELEM[MemLen];
+	for (int i = 0; i<MemLen; i++)
+		pMem[i] = 0;
 }
+enum ERRORS { BADSIZE, BADINDEX };
 
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
 {
@@ -21,21 +28,30 @@ TBitField::~TBitField()
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
+	return n / (sizeof(TELEM) * 8);
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
 {
+	int pos = n % (sizeof(TELEM) * 8);
+	TELEM tmp = 1;
+	tmp <<= pos;
+	return tmp;
 }
 
 // доступ к битам битового поля
 
 int TBitField::GetLength(void) const // получить длину (к-во битов)
 {
-  return 0;
+	return 0;
 }
 
 void TBitField::SetBit(const int n) // установить бит
 {
+	if (n <= 0 || n > BitLen) throw BADINDEX;
+	int index = GetMemIndex(n);
+	TELEM mask = GetMemMask(n);
+	pMem[index] |= mask;
 }
 
 void TBitField::ClrBit(const int n) // очистить бит
@@ -44,23 +60,24 @@ void TBitField::ClrBit(const int n) // очистить бит
 
 int TBitField::GetBit(const int n) const // получить значение бита
 {
-  return 0;
+	return 0;
 }
 
 // битовые операции
 
 TBitField& TBitField::operator=(const TBitField &bf) // присваивание
 {
+	return *this;
 }
 
 int TBitField::operator==(const TBitField &bf) const // сравнение
 {
-  return 0;
+	return 0;
 }
 
 int TBitField::operator!=(const TBitField &bf) const // сравнение
 {
-  return 0;
+	return 0;
 }
 
 TBitField TBitField::operator|(const TBitField &bf) // операция "или"
@@ -77,10 +94,15 @@ TBitField TBitField::operator~(void) // отрицание
 
 // ввод/вывод
 
-istream &operator>>(istream &istr, TBitField &bf) // ввод
+istream &operator >> (istream &istr, TBitField &bf) // ввод
 {
 }
 
 ostream &operator<<(ostream &ostr, const TBitField &bf) // вывод
 {
+	for (int i = 0; i < bf.GetLength; i++)
+	{
+		ostr << bf.fNames[i] << " = " << bf.GetBit(bf.fNames[i]) << endl;
+	}
+	return ostr;
 }
